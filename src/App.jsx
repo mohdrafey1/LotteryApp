14afac14a18fb07e61fced01b76f7a083048d8ff
Mohd Rafey <mohdrafey600@gmail.com>
2024-04-05 08:19:12 +0530

    remove 1 wincondition

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { sum } from './components/Lottery/helper';
 function App() {
     let winCondition = (ticket) => {
         const ticketSum = sum(ticket);
-        return ticketSum === 15 || ticketSum === 10 || ticketSum === 20;
+        return ticketSum === 15 || ticketSum === 10;
     };
 
     return (
